e6bcf1eef943cf923f8192abf5695e386fcac2e2
laihoj <jaakkolaiho94@gmail.com>
2018-04-10 22:32:45 +0300

    testing routing

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require("express"),
 
 app.set("view engine", "ejs");
 
+var commonRoute = require("./routes/common");
+app.use(commonRoute);
+
 app.get("/*", function(req,res) {
 	res.render("index");
 });
