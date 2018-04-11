1b2f9bd68b8fac521bfe5bbb285bd4adb11e00cf
laihoj <jaakkolaiho94@gmail.com>
2018-04-11 09:01:49 +0300

    end of day commmit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require("express"),
+	bodyParser = require("body-parser");
 	app		= express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
-var commonRoute = require("./routes/common");
+var commonRoute = require("./routes/commonRoutes");
 app.use(commonRoute);
 
 app.get("/*", function(req,res) {
---
+++ routes/commonRoutes.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+//========================================================
+//BASIC ROUTES 
+//========================================================
+
+router.get("/", function(req,res){
+	res.render("common/index");
+});
+
+router.get("/campaigns", function(req,res){
+	res.render("common/campaigns");
+});
+
+router.get("/campaigns/:id", function(req,res) {
+	res.render("common/campaign", {id:req.params.id});
+});
+
+router.get("/rewards", function(req,res){
+	res.render("common/rewards");
+});
+
+router.get("/rewards/:id", function(req,res){
+	res.render("common/reward", {id:req.params.id});
+});
+
+router.get("/register", function(req,res){
+	res.render("common/register");
+});
+
+router.get("/login", function(req,res){
+	res.render("common/login");
+});
+
+
+module.exports = router;
