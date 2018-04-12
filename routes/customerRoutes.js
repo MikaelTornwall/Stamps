3b1f60e05c3131cc75d11d307de0215c445af5e9
laihoj <jaakkolaiho94@gmail.com>
2018-04-12 23:53:57 +0300

    added cards view

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -15,10 +15,12 @@ router.get("/:id", function(req,res){
 	res.render("customer/profile", {id:req.params.id});
 });
 
+router.get("/:id/cards", function(req, res) {
+	res.render("customer/cards", {id:req.params.id});
+});
+
 router.get("/:id/cards/:stampcard", function(req, res) {
 	res.render("customer/stampcard", {stampcard:req.params.stampcard,id:req.params.id});
 });
-
-
 
 module.exports = router;
