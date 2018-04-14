3f878e4841743b631108fdf9d64aff606d3b7212
Mikael Törnwall <mikael.tornwall@aalto.fi>
2018-04-14 17:14:48 +1200

    Css file, bootstrap

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 var url = process.env.DATABASEURL || "mongodb://localhost/stamps";
 mongoose.connect(url);
