5187208e2a6b236fbbb4f2b828b7ea8ddd2a3405
laihoj <jaakkolaiho94@gmail.com>
2018-04-18 13:11:29 +0300

    fixed stylesheet availability issue, cleaned up some code

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + "/public/"));
+// app.use(express.static(__dirname + "/public/"));
+app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 
